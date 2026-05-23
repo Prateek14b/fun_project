e19675d248c4e372aca06833c122c9dccc4d7de2
Prateek <50728753+Prateek14b@users.noreply.github.com>
2026-05-23 23:05:02 +0530

    combined data frames from different years into a dictionary. Inserted a new "years" column in each data frame that filled out every cell under it with the same year (2015/2016/2017/etc.)

--- fun_project.py
+++ fun_project.py
@@ -88,3 +88,17 @@ def load_2019(path):
 #1 ----- created functions for csv -> dataframes for each year! ------
 
 
+combined = { #combined dataframes into a quasi-dictionary for convenience.
+    "2015": load_2015("~/archive (1)/2015.csv"),
+    "2016": load_2016("~/archive (1)/2016.csv"),
+    "2017": load_2017("~/archive (1)/2017.csv"),
+    "2018": load_2018("~/archive (1)/2018.csv"),
+    "2019": load_2019("~/archive (1)/2019.csv")
+}
+
+for year, df in combined.items():
+    df.insert(0, "year", year) #we insert a new column for each year's dataframe in column position 1, and fill out all rows/records under that column with 1 value: the corresponding year in question!
+
+#2 ----- combined dataframes and inserted year column so the combining makes more sense!--------
+
+
