c934d77accc702495999c5e28b0f077b42405619
Prateek <50728753+Prateek14b@users.noreply.github.com>
2026-05-26 13:49:50 +0530

    added CORS middleware to define which port to allow HTTPS requests from, and block any other ports' attempts to communicate with FastAPI.

--- main.py
+++ main.py
@@ -2,5 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import create_engine, text
 
-app = FastAPI()
+app = FastAPI() #creates a fastAPI app instance.
+
+#now we use CORS (Cross-Origin Resource Sharing), which is a browser security rule that blocks
+#requests coming from different ports than what we allowed.
+#This middleware tells FASTAPI to explicitly allow requests only from port 300 (the default React port)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], #only allow requests from React APP
+    allow_methods=["GET"], #only allow GET requests
+    allow_headers=["*"], #allow any headers
+)
 
