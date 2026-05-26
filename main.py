0c0f2f15dac2d6008c4d994ab26da0964e38d1a2
Prateek <50728753+Prateek14b@users.noreply.github.com>
2026-05-26 14:20:41 +0530

    define security protocols to prevent SQL injections and the like, by restricting query parameters to the columns of the happiness table.

--- main.py
+++ main.py
@@ -14,3 +14,30 @@ app.add_middleware(
     allow_headers=["*"], #allow any headers
 )
 
+#-----Database connection via SQLAlchemy's create_engine(); we use postgres container's unique database URL to securely connect.----
+#-----port 5432 is whene Docker is exposing Postgres container on ----
+engine = create_engine("postgresql://admin:password@localhost:5432/happinessdb")
+
+# --- Routes -----------
+# @app.get("/api/stats") is the standard form for a HTTP request to fastAPI
+#we need to define the script/behavior for converting the GET request 
+#into a valid SQL query!
+#so we define a function get_stats() that takes care of precisely this conversion.
+
+#FASTapi automatically parses the incoming GET request, reads the required request and fills in the arguments for get_stats() accordingly.
+@app.get("/api/stats")
+def get_stats(year: int=2019, region: str=None, metric: str= "happiness_score"):
+
+    #first things first, we have to prevent SQL injections, so we
+    #define only the allowed columns for the user to query over.
+
+    allowed_metrics = {
+        "happiness_score", "gdp_per_capita", "social_support", 
+        "life_expectancy", "freedom", "corruption", "generosity"
+    }
+
+    if metric not in allowed_metrics:
+        metric = "happiness_score" #fall back to default column/metric if an invalid metric is specified in request.
+
+    
+    
