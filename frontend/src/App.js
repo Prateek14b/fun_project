eb5a19ce15d3f32d6716b5e8ba1dffbacd65d6d0
Prateek <50728753+Prateek14b@users.noreply.github.com>
2026-05-27 12:45:20 +0530

    mapping region names to different colors, in a dict!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,24 @@
-import { useState, useEffect} from "react";
-import axios from "axios"
+import { useState, useEffect} from "react"; //these are React hooks. 
+import axios from "axios" //axios is a library for making HTTP requests -- cleaner than raw fetch()
 import {
   BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell
 } from "recharts"
+
+const API = "http://localhost:8000"; //URL for fastAPI backend; all API calls' endpoints point to this.
+
+/* map each region name to a hex color, and store the
+mapping in a lookup dict for looking up later.*/
+const REGION_COLORS = {
+  "Western Europe": "#4e79a7",
+  "North America": "#f28e2b",
+  "Australia and New Zealand": "#59a14f",
+  "Latin America and Caribbean": "#e15759",
+  "Eastern Asia": "#76b7b2",
+  "Southeastern Asia": "#edc948",
+  "Central and Eastern Europe": "#b07aa1",
+  "Middle East and Northern Africa": "#ff9da7",
+  "Sub-Saharan Africa": "#9c755f",
+  "Southern Asia": "#bab0ac",
+};
+
 
