27b4e31e08aa4cadefa5b21ffe4905dca82a913f
Prateek <50728753+Prateek14b@users.noreply.github.com>
2026-05-27 15:29:06 +0530

    modified tailwind.config.js, particularly in the place where we define 'content: []' that tells Tailwind to look in those areas to resolve any unknown classes!

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [ //tells Tailwind which locations to look for any classes!(we would prolly only use classes within src parent directory so we define that here.)
+    "./src/**/*.{js, jsx, ts, tsx}",
+    // **/* means "any folder, any depth" inside src/
+    // *.{js,jsx,ts,tsx} means "any file with these extensions"
+    // So it scans every JS/JSX file in your src folder
+  ],
   theme: {
     extend: {},
   },
