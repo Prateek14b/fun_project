c839062b1f1e47c194d3ad9d8345a89128b07859
Prateek <50728753+Prateek14b@users.noreply.github.com>
2026-05-27 11:55:48 +0530

    time to work on the front-end webpage that connects to the FastAPI endpoints.
    1st, we import the necessary libaries.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from "react";
+import axios from "axios"
+import {
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell
+} from "recharts"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
